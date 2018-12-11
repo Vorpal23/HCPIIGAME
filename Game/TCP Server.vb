61c99ae700d56c711b2566b6b29f6d40e0706d86
Jordan Rose <31141432+Vorpal23@users.noreply.github.com>
2018-12-11 12:19:22 -0500

    Update TCP Server.vb

--- Game/TCP Server.vb
+++ Game/TCP Server.vb
@@ -1,3 +1,14 @@
-﻿Public Class Class1
+﻿Imports System.Net.Sockets
+Imports System.Runtime.Serialization
+
+
+
+Public Class Class1
+    Dim Server As New TcpListener(8888)
+    Dim client As TcpClient
+    Sub Idk()
+        client = Server.AcceptTcpClient()
+
+    End Sub
 
 End Class
