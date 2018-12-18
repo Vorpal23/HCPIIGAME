6a418b626776345547f6416640c174654344bc62
Jordan Rose <31141432+Vorpal23@users.noreply.github.com>
2018-12-18 11:46:55 -0500

    fix
    Send

--- Game/Form1.vb
+++ Game/Form1.vb
@@ -3,6 +3,7 @@ Imports System.IO
 Imports System.Net
 Imports System.Threading
 Public Class Form1
+    Dim c As PlayerAva
     Dim mp As New Multiplayer(InputBox("path"), 2)
     Dim key As KeyPressEventArgs
     Dim hc As Boolean
@@ -26,10 +27,10 @@ Public Class Form1
         }
     End Sub
     Private Sub Form1_KeyPress(sender As Object, e As KeyPressEventArgs) Handles Me.KeyPress
-        'PlayerAva1.Movement(e)
+        'c.Movement(e)
         key = e
-        PlayerAva1.keyPressed = True
-        ' If (PlayerAva1.Left + PlayerAva1.Width) > Wallex1.Left Then
+        c.keyPressed = True
+        ' If (c.Left + c.Width) > Wallex1.Left Then
         'MsgBox("T")
         'End If
 
@@ -41,20 +42,20 @@ Public Class Form1
     End Sub
 
     Private Sub Wallex1_Click(sender As Object, e As EventArgs) Handles Wallex1.Click
-        Hitpointsbar1.Update(PlayerAva1)
+        Hitpointsbar1.Update(c)
     End Sub
 
     Private Sub Wallex1_DoubleClick(sender As Object, e As EventArgs) Handles Wallex1.DoubleClick
-        PlayerAva1.Hitpoints += -1
-        Hitpointsbar1.Update(PlayerAva1)
+        c.Hitpoints += -1
+        Hitpointsbar1.Update(c)
     End Sub
 
     Private Sub Tm1_Tick(sender As Object, e As EventArgs) Handles Tm1.Tick
         '  Dim ListenerThread As New Thread(New ThreadStart(AddressOf Host_Start))
         ' If Not ListenerThread.IsAlive Then ListenerThread.Start()
 
-        If PlayerAva1.keyPressed = True Then
-            PlayerAva1.Movement(key)
+        If c.keyPressed = True Then
+            c.Movement(key)
         End If
     End Sub
     Sub Host_Start()
@@ -62,12 +63,13 @@ Public Class Form1
     End Sub
 
     Private Sub Form1_KeyUp(sender As Object, e As KeyEventArgs) Handles Me.KeyUp
-        PlayerAva1.keyPressed = False
+        c.keyPressed = False
     End Sub
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Hitpointsbar1.Update(PlayerAva1)
-        PlayerAva1.Mvmspeed = 5
+        c = PlayerAva2
+        Hitpointsbar1.Update(c)
+        c.Mvmspeed = 5
         Chose()
         SetControl()
 
@@ -76,7 +78,7 @@ Public Class Form1
     End Sub
 
     Private Sub btnBattleUI_Click(sender As Object, e As EventArgs) Handles btnBattleUI.Click
-        Battleui1.inta(PlayerAva1)
+        Battleui1.inta(c)
         Battleui1.Width = Me.Width
         Battleui1.Height = Me.Height
         Battleui1.Left = 0
@@ -84,7 +86,7 @@ Public Class Form1
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        PlayerAva1.type = TextBox1.Text
+        c.type = TextBox1.Text
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
@@ -100,5 +102,9 @@ Public Class Form1
 
     Private Sub btTake_Click(sender As Object, e As EventArgs) Handles btTake.Click
         mp.Take_turn(PlayerAva1, PlayerAva2, PlayerAva3)
+    End Sub
+
+    Private Sub Battleui1_Load(sender As Object, e As EventArgs) Handles Battleui1.Load
+
     End Sub
 End Class
